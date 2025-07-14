4f5afc121bab45e2e829f07038e17b44fb5eb9f9
Ugluk <git@ugluk.dev>
2025-07-14 09:28:52 +0200

    added file browser options

--- lua/custom/plugins/config/file_browser.lua
+++ lua/custom/plugins/config/file_browser.lua
@@ -1,22 +1,27 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
 --
 -- You don't need to set any of these options.
 -- IMPORTANT!: this is only a showcase of how you can set default options!
-require("telescope").setup {
+local fb_actions = require 'telescope._extensions.file_browser.actions'
+require('telescope').setup {
   extensions = {
     file_browser = {
-      theme = "ivy",
-      -- disables netrw and use telescope-file-browser in its place
-      hijack_netrw = true,
+      mappings = {
+        ['i'] = {
+          ['<C-h>'] = fb_actions.goto_home_dir,
+          ['<C-x>'] = function()
+            print 'test'
+          end,
+        },
+      },
     },
+    theme = 'ivy',
+    -- disables netrw and use telescope-file-browser in its place
+    hijack_netrw = true,
   },
 }
 
 -- To get telescope-file-browser loaded and working with telescope,
 -- you need to call load_extension, somewhere after setup function:
-require("telescope").load_extension "file_browser"
-
-
+require('telescope').load_extension 'file_browser'
